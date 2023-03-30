51afcf38a1b881ffd18a26ec4b1e96a87e1bd52d
SzaeraM <129379084+SzaeraM@users.noreply.github.com>
2023-03-30 14:05:14 +0200

    Upload Directories

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(name='green/fl',
+      packages=find_packages(),
+      version='0.0.1',
+      description='Green Python package',
+      author='HI-Iberia')
