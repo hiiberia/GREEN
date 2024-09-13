4cefdaedb133ffaed86a9d16e8d07008ef90a3fb
Sergio Zaera Mata <szaera@hi-iberia.es>
2024-09-13 16:57:25 +0200

    chore(repo): remove unused folders and files

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(name='green/fl',
-      packages=find_packages(),
-      version='0.0.1',
-      description='Green Python package',
-      author='HI-Iberia')
